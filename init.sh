4e1036ecb0dce34fb9146c2e6003999918c3fa8d
weiyu <weiyu@unitedstack.com>
2016-04-07 16:06:33 +0800

    rf init.sh

--- init.sh
+++ init.sh
@@ -1,27 +1,33 @@
-cd /root
-git clone https://github.com/openstack/kolla.git
-cd kolla 
-git fetch origin stable/mitaka:mitaka
-git checkout mitaka
-yum install epel-release -y
-yum install python-pip -y
-yum install -y python-devel libffi-devel openssl-devel gcc git
+#!/bin/bash
+
+TOPDIR=$(cd $(dirname "$0") && pwd)
+
+# 安装必要的包
+yum install -y epel-release 
+yum install -y python-devel libffi-devel openssl-devel gcc git python-pip ansible
 pip install -U python-openstackclient
-
-cd /root
-pip install kolla/
-
-cd kolla
-cp -r etc/kolla /etc/
-yum -y install ansible
 pip install -U ansible==1.9.4
 
-cd /root/kolla-base
+# 获取kolla代码
+pushd "${TOPDIR}" > /dev/null
+git clone https://github.com/openstack/kolla.git
+pushd "${TOPDIR}/kolla" > /dev/null
+git fetch origin stable/mitaka:mitaka
+git checkout mitaka
+cp -r etc/kolla /etc/
+popd > /dev/null
+
+# 安装kolla
+pip install kolla/
+
+# 配置kolla
+pushd "${TOPDIR}/kolla-base" > /dev/null
 \cp config/* /etc/kolla/
 \cp ansible_hosts /etc/ansible/hosts
 \cp hosts /etc/hosts
-cd ansible
-ansible-playbook -e @/etc/kolla/globals.yml -e action=prechecks -e @/etc/kolla/passwords.yml site.yml
 
-cd /root
+# 初始化目标节点
+ansible-playbook -e @/etc/kolla/globals.yml -e @/etc/kolla/passwords.yml ansible/site.yml
+
+# 部署openstack集群
 kolla-ansible deploy -i /etc/ansible/hosts
